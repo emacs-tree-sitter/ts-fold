c33e5e7e0c4cf08325a38c7b0408f959169f3dca
Jen-Chieh Shen <jcs090218@gmail.com>
2021-10-07 01:01:34 +0800

    Temporily disable csharp

--- tree-sitter-fold-parsers.el
+++ tree-sitter-fold-parsers.el
@@ -85,12 +85,12 @@
     (switch_body                          . tree-sitter-fold-range-seq)
     (anonymous_object_creation_expression . tree-sitter-fold-range-seq)
     (initializer_expression               . tree-sitter-fold-range-seq)
-    (if_directive                         . tree-sitter-fold-range-seq)
-    (else_directive                       . tree-sitter-fold-range-seq)
-    (elif_directive                       . tree-sitter-fold-range-seq)
-    (endif_directive                      . tree-sitter-fold-range-seq)
-    (region_directive                     . tree-sitter-fold-range-seq)
-    (endregion_directive                  . tree-sitter-fold-range-seq)
+    ;;(if_directive                         . tree-sitter-fold-range-seq)
+    ;;(else_directive                       . tree-sitter-fold-range-seq)
+    ;;(elif_directive                       . tree-sitter-fold-range-seq)
+    ;;(endif_directive                      . tree-sitter-fold-range-seq)
+    ;;(region_directive                     . tree-sitter-fold-range-seq)
+    ;;(endregion_directive                  . tree-sitter-fold-range-seq)
     (comment                              . tree-sitter-fold-range-c-like-comment)))
 
 (defun tree-sitter-fold-parsers-css ()
@@ -164,7 +164,9 @@
 
 (defun tree-sitter-fold-parsers-ruby ()
   "Rule sets for Ruby."
-  '((if . (tree-sitter-fold-range-seq 1 -2))))
+  '((method  . tree-sitter-fold-range-ruby-method)
+    (comment . (lambda (node offset)
+                 (tree-sitter-fold-range-line-comment node offset "#")))))
 
 (defun tree-sitter-fold-parsers-rust ()
   "Rule sets for Rust."
